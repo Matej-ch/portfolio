cf80c900d5c9ee1f6d810de3a252b3b7d21c5cdb
Matej <matejchalachan@gmail.com>
2021-05-26 20:08:10 +0200

    initial routes

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController
 {
-    public function index()
+
+    /**
+     * @Route("/")
+     *
+     * @return Response
+     */
+    public function homepage(): Response
     {
         return new Response('Project controller');
+    }
+
+    /**
+     * @Route("/projects/{slug}")
+     */
+    public function show($slug): Response
+    {
+        return new Response(sprintf('Showing specific project "%s"!',ucwords(str_replace('-',' ',$slug))));
     }
 }
