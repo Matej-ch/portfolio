b1defe8d28de46a89671cd7ddf47ecb8fff6117d
Matej <matejchalachan@gmail.com>
2021-05-27 21:02:48 +0200

    init for css an js

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +13,19 @@ class ProjectController extends AbstractController
 {
 
     /**
-     * @Route("/")
-     *
-     * @return Response
+     * @Route("/",name="app_homepage")
      */
     public function homepage(): Response
     {
-        return new Response('Project controller');
+        return $this->render('project/homepage.html.twig');
     }
 
     /**
-     * @Route("/projects/{slug}")
+     * @Route("/projects/{slug}",name="app_show")
      */
-    public function show($slug): Response
+    public function show($slug, LoggerInterface $logger): Response
     {
+        $logger->info("Showing project $slug");
         return $this->render('project/show.html.twig',[
             'question' => ucwords(str_replace('-',' ',$slug)),
             'answers' =>[1,2,3]
