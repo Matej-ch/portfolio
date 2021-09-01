24516119c5f0844079c111581dda7272f48a8a8c
Matej <matejchalachan@gmail.com>
2021-09-01 17:07:54 +0200

    navbar updated

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -8,34 +8,47 @@ export default function Navbar({ fixed }) {
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-500 mb-3">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a
-                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                            href=""
-                        >
-                            lightBlue Tailwind Starter Kit
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                            href="">
+                            Projects
                         </a>
+
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                           href="">
+                            <i className="fas fa-gamepad text-lg leading-lg text-white opacity-75"/>
+                            <span className="ml-2">Games</span>
+                        </a>
+
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                           href="">
+                            <i className="fas fa-cubes text-lg leading-lg text-white opacity-75"/>
+                            <span className="ml-2">Packages</span>
+                        </a>
+
                         <button
                             className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                             type="button"
-                            onClick={() => setNavbarOpen(!navbarOpen)}
-                        >
+                            onClick={() => setNavbarOpen(!navbarOpen)}>
                             <i className="fas fa-bars"/>
                         </button>
                     </div>
                     <div
-                        className={
-                            "lg:flex flex-grow items-center" +
-                            (navbarOpen ? " flex" : " hidden")
-                        }
-                        id="example-navbar-danger"
-                    >
+                        className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")}
+                        id="example-navbar-danger">
                         <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                             <li className="nav-item">
                                 <a
                                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                                     href=""
                                 >
-                                    <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"/><span className="ml-2">Share</span>
+                                    <i className="fab fa-linkedin text-lg leading-lg text-white opacity-75"/><span className="ml-2">LinkedIn</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    href="">
+                                    <i className="fab fa-github text-lg leading-lg text-white opacity-75"/><span className="ml-2">Github</span>
                                 </a>
                             </li>
                             <li className="nav-item">
@@ -43,15 +56,7 @@ export default function Navbar({ fixed }) {
                                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                                     href=""
                                 >
-                                    <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"/><span className="ml-2">Tweet</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                                    href=""
-                                >
-                                    <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"/><span className="ml-2">Pin</span>
+                                    <i className="fas fa-user text-lg leading-lg text-white opacity-75"/><span className="ml-2">About me</span>
                                 </a>
                             </li>
                         </ul>
