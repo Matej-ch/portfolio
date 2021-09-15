6153e7b223773e0abd4f3e1c1e10929d5997af7a
Matej <matejchalachan@gmail.com>
2021-09-15 21:08:16 +0200

    external site show icon in index

--- src/Controller/Admin/ExternalSiteCrudController.php
+++ src/Controller/Admin/ExternalSiteCrudController.php
@@ -5,9 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\ExternalSite;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class ExternalSiteCrudController extends AbstractCrudController
 {
+
     public static function getEntityFqcn(): string
     {
         return ExternalSite::class;
@@ -20,5 +23,16 @@ class ExternalSiteCrudController extends AbstractCrudController
             ->setEntityLabelInPlural('External sites')
             ->setSearchFields(['name', 'url'])
             ->setDefaultSort(['ordering' => 'ASC']);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            'name',
+            UrlField::new('url'),
+            ImageField::new('icon')->setUploadDir('public/uploads/sites')->hideOnIndex(),
+            ImageField::new('icon')->setBasePath('uploads/sites')->hideOnForm(),
+            'ordering'
+        ];
     }
 }
