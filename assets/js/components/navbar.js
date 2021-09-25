bdb60aac55e3dd3c4a3bd59986f6790e8708ba60
Matej <matejchalachan@gmail.com>
2021-09-25 21:51:23 +0200

    navbar updated for mobiles

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -14,13 +14,13 @@ const Navbar = ({ fixed,github,linkedin }) => {
                             Projects
                         </a>
 
-                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                        <a className="text-sm font-bold leading-relaxed hidden mr-4 py-2 whitespace-nowrap uppercase text-white md:inline-block"
                            href="/projects">
                             <i className="fas fa-gamepad text-lg leading-lg text-white opacity-75"/>
                             <span className="ml-2">Games</span>
                         </a>
 
-                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                        <a className="text-sm font-bold leading-relaxed hidden mr-4 py-2 whitespace-nowrap uppercase text-white md:inline-block"
                            href="/projects">
                             <i className="fas fa-cubes text-lg leading-lg text-white opacity-75"/>
                             <span className="ml-2">Packages</span>
@@ -36,22 +36,22 @@ const Navbar = ({ fixed,github,linkedin }) => {
                     <div
                         className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")}
                         id="example-navbar-danger">
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                            <li className="nav-item">
+                        <ul className="flex flex-row md:flex-row list-none lg:ml-auto">
+                            <li>
                                 <a
                                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                                     href={linkedin}>
                                     <i className="fab fa-linkedin text-lg leading-lg text-white opacity-75"/>
                                 </a>
                             </li>
-                            <li className="nav-item">
+                            <li>
                                 <a
                                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                                     href={ github }>
                                     <i className="fab fa-github text-lg leading-lg text-white opacity-75"/>
                                 </a>
                             </li>
-                            <li className="nav-item">
+                            <li>
                                 <a
                                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                                     href="/about">
