e4720c45ec9894662e7c319a115c547f37068b48
Matej <matejchalachan@gmail.com>
2021-09-25 22:13:12 +0200

    removed deprecated code,added links

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Language;
-use App\Form\LanguageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,71 +14,5 @@ class LanguageController extends AbstractController
         return $this->render('language/index.html.twig', [
             'controller_name' => 'LanguageController',
         ]);
-    }
-
-    #[Route('/language/new', name: 'language_new')]
-    public function new(Request $request, string $iconDir): Response
-    {
-        $language = new Language();
-
-        $form = $this->createForm(LanguageType::class, $language);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $language = $form->getData();
-
-            if ($icon = $form['icon']->getData()) {
-                $filename = bin2hex(random_bytes(6)).'.'.$icon->guessExtension();
-                try {
-                    $icon->move($iconDir, $filename);
-                } catch (FileException $e) {
-                    // unable to upload the photo, give up
-                }
-                $language->setPhotoFilename($filename);
-            }
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($language);
-            $entityManager->flush();
-
-            $this->addFlash('success', "Language ".$language->getType()." Created!");
-
-            return $this->redirectToRoute('language');
-        }
-
-        return $this->render('language/new.html.twig',['form' => $form->createView()]);
-    }
-
-    #[Route('/language/{id}/edit', name: 'language_edit')]
-    public function edit(Language $language,Request $request, string $iconDir): Response
-    {
-        $form = $this->createForm(LanguageType::class, $language);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()) {
-
-            //$language = $form->getData();
-
-            if ($icon = $form['icon']->getData()) {
-                $filename = bin2hex(random_bytes(6)).'.'.$icon->guessExtension();
-                try {
-                    $icon->move($iconDir, $filename);
-                } catch (FileException $e) {
-                    // unable to upload the photo, give up
-                }
-                $language->setPhotoFilename($filename);
-            }
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($language);
-            $entityManager->flush();
-
-            $this->addFlash('success', "Language type ".$language->getType()." Updated!");
-
-            return $this->redirectToRoute('language');
-        }
-
-        return $this->render('language/edit.html.twig',['form' => $form->createView(),'language' => $language]);
     }
 }
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,11 +5,8 @@ namespace App\Controller;
 
 
 use App\Entity\Project;
-use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +17,6 @@ class ProjectController extends AbstractController
     #[Route('/projects', name: 'app_project')]
     public function homepage(Request $request,ProjectRepository $repository): Response
     {
-        $projects = $repository->findAllOrderByNewest();
-
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $repository->getProjectPaginator($offset);
 
@@ -32,49 +27,11 @@ class ProjectController extends AbstractController
         ]);
     }
 
-    #[Route('/projects/new', name: 'project_new')]
-    public function new(Request $request): Response
-    {
-        $project = new Project();
-        $project->setIsActive(1);
-        $project->setName('First project');
-        $project->setDescription('First project description');
-
-        $form = $this->createForm(ProjectType::class, $project);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $project = $form->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($project);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_homepage');
-        }
-
-        return $this->render('project/new.html.twig',['form' => $form->createView()]);
-    }
-
     #[Route('/projects/{slug}', name: 'project_show')]
     public function show(Project $project): Response
     {
         return $this->render('project/show.html.twig',[
             'project' => $project,
-        ]);
-    }
-
-    #[Route('/projects/{slug}/update', name: 'project_update', methods: "POST")]
-    public function updateDescription(Project $project,Request $request,EntityManagerInterface $entityManager): RedirectResponse
-    {
-        $description = $request->request->get('description');
-
-        $project->setDescription($description);
-
-        $entityManager->flush();
-
-        return $this->redirectToRoute('project_show',[
-            'slug' => $project->getSlug()
         ]);
     }
 }
