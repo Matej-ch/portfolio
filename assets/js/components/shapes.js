e8d2ebfe13a18282add784f2fb88305eb927740d
Matej <matejchalachan@gmail.com>
2021-10-03 14:26:48 +0200

    added shapes background to admin login

---
+++ assets/js/components/shapes.js
@@ -0,0 +1,22 @@
+import SBackgroundCreator from "bg_shapes/src/js/SBackgroundCreator";
+
+window.onload = function () {
+    const canvasEl = document.getElementById('canvas');
+
+    const parentEl = canvasEl.parentElement;
+
+    canvasEl.width = parentEl.offsetWidth;
+    canvasEl.height = parentEl.offsetHeight;
+
+    const bg = new SBackgroundCreator(canvasEl,{
+        fillShape: true,
+        numShapes: 8,
+        bgColor:'rgba(243, 244, 246, 1)'
+    });
+
+    bg.deactivateShapes(['Heart','Triangle','Line','Wave']);
+
+    bg.init();
+
+    bg.draw();
+}
