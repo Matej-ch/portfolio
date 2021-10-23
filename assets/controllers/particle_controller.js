1d8b150e55a9de8292faa63dfd7875797441ee5e
Matej <matejchalachan@gmail.com>
2021-10-23 21:48:30 +0200

    paritcle settings boudned

--- assets/controllers/particle_controller.js
+++ assets/controllers/particle_controller.js
@@ -4,22 +4,31 @@ import PBackground from "bg_particles/src/js/PBackground";
 export default class extends Controller {
     static targets = ['canvas','settingBtn','settingMenu'];
 
-    connect() {
+    connect()
+    {
         this.initializeParticles();
     }
 
-    showMenu() {
+    showMenu()
+    {
         this.settingBtnTarget.classList.add('hidden');
         this.settingMenuTarget.classList.remove('hidden');
     }
 
-    hideMenu() {
+    hideMenu()
+    {
         this.settingMenuTarget.classList.add('hidden');
         this.settingBtnTarget.classList.remove('hidden');
     }
 
-    initializeParticles() {
-        const bg = new PBackground({
+    updateBackground(event)
+    {
+        this.initializeParticles({[event.currentTarget.dataset.attribute]: parseInt(event.currentTarget.value)});
+    }
+
+    initializeParticles(options = {}) {
+
+        let bgOptions = {
             canvasSelector: `#${this.canvasTarget.id}`,
             bgColor:'linear-gradient(0.15turn, rgb(223, 185, 106, 1), rgb(135, 190, 231, 1)90% )',
             canvasW: window.innerWidth,
@@ -27,7 +36,12 @@ export default class extends Controller {
             speedMod: 0.2,
             particleCount:200,
             lineModifier: 10000,
-            lineColor: [0, 84, 219]});
+            lineColor: [0, 84, 219]
+        }
+
+        bgOptions = {...bgOptions,...options};
+
+        const bg = new PBackground(bgOptions);
 
         /** initialize particles */
         bg.init();
