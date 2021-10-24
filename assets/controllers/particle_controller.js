a7af8ae639c4e87f288d8b6a4b204b0415625d15
Matej <matejchalachan@gmail.com>
2021-10-24 21:52:44 +0200

    particle change partially updated

--- assets/controllers/particle_controller.js
+++ assets/controllers/particle_controller.js
@@ -2,6 +2,7 @@ import { Controller } from 'stimulus';
 import PBackground from "bg_particles/src/js/PBackground";
 
 export default class extends Controller {
+    bg = null;
 
     static targets = ['canvas','settingBtn','settingMenu'];
 
@@ -13,11 +14,29 @@ export default class extends Controller {
 
     connect()
     {
-        this.initializeParticles({
-            speedMod: this.speedModValue,
-            particleCount: this.particleCountValue,
-            lineModifier: this.lineModifierValue
-        });
+        let bgOptions = {
+            canvasSelector: `#${this.canvasTarget.id}`,
+            bgColor:'linear-gradient(0.15turn, rgb(223, 185, 106, 1), rgb(135, 190, 231, 1)90% )',
+            canvasW: window.innerWidth,
+            canvasH: this.element.clientHeight,
+            speedMod: 0.2,
+            particleCount:200,
+            lineModifier: 10000,
+            lineColor: [0, 84, 219]
+        }
+
+        bgOptions = {...bgOptions,...{
+                speedMod: this.speedModValue,
+                particleCount: this.particleCountValue,
+                lineModifier: this.lineModifierValue
+            }};
+
+        this.bg = new PBackground(bgOptions);
+
+        this.initializeParticles(this.bg);
+
+        /** start animation */
+        this.bg.animate();
     }
 
     showMenu()
@@ -34,32 +53,12 @@ export default class extends Controller {
 
     updateBackground(event)
     {
-        this.initializeParticles({[event.currentTarget.dataset.attribute]: parseInt(event.currentTarget.value)});
+        this.initializeParticles(this.bg);
     }
 
-    initializeParticles(options = {}) {
-
-        let bg = {};
-
-        let bgOptions = {
-            canvasSelector: `#${this.canvasTarget.id}`,
-            bgColor:'linear-gradient(0.15turn, rgb(223, 185, 106, 1), rgb(135, 190, 231, 1)90% )',
-            canvasW: window.innerWidth,
-            canvasH: this.element.clientHeight,
-            speedMod: 0.2,
-            particleCount:200,
-            lineModifier: 10000,
-            lineColor: [0, 84, 219]
-        }
-
-        bgOptions = {...bgOptions,...options};
-
-        bg = new PBackground(bgOptions);
+    initializeParticles(bg) {
 
         /** initialize particles */
         bg.init();
-
-        /** start animation */
-        bg.animate();
     }
 }
