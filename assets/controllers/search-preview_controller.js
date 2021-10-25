4366532791dac75c107cb9a8986125eda1dee81b
Matej <matejchalachan@gmail.com>
2021-10-25 21:52:56 +0200

    added debounce and click outside with stimulus package

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,4 +1,6 @@
 import { Controller } from 'stimulus';
+import { useClickOutside } from 'stimulus-use'
+import { useDebounce } from 'stimulus-use'
 
 export default class extends Controller {
 
@@ -8,10 +10,26 @@ export default class extends Controller {
 
     static targets = ['result']
 
+    static debounces = ['search']
+
+    connect() {
+        useClickOutside(this);
+        useDebounce(this);
+    }
+
+    clickOutside(event) {
+        event.preventDefault()
+        this.resultTarget.innerHTML = '';
+    }
+
     async onSearchInput(e)
     {
+        this.search(e.currentTarget.value);
+    }
+
+    async search(query) {
         const params = new URLSearchParams({
-            q: e.currentTarget.value,
+            q: query,
             preview: '1'
         });
 
