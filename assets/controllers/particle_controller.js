d5243837dd62029121748957c515bb2343e91c0b
Matej <matejchalachan@gmail.com>
2021-10-27 20:22:32 +0200

    attempt to fix bg particle reload

--- assets/controllers/particle_controller.js
+++ assets/controllers/particle_controller.js
@@ -3,6 +3,7 @@ import PBackground from "bg_particles/src/js/PBackground";
 
 export default class extends Controller {
     bg = null;
+    bgOptions = {};
 
     static targets = ['canvas','settingBtn','settingMenu'];
 
@@ -14,24 +15,25 @@ export default class extends Controller {
 
     connect()
     {
-        let bgOptions = {
+        this.bgOptions = {
             canvasSelector: `#${this.canvasTarget.id}`,
             bgColor:'linear-gradient(0.15turn, rgb(223, 185, 106, 1), rgb(135, 190, 231, 1)90% )',
             canvasW: window.innerWidth,
             canvasH: this.element.clientHeight,
-            speedMod: 0.2,
+            speedMod: 5,
             particleCount:200,
             lineModifier: 10000,
             lineColor: [0, 84, 219]
         }
 
-        bgOptions = {...bgOptions,...{
+        this.bgOptions = {...this.bgOptions,...{
                 speedMod: this.speedModValue,
                 particleCount: this.particleCountValue,
                 lineModifier: this.lineModifierValue
             }};
 
-        this.bg = new PBackground(bgOptions);
+        //console.log(this.bgOptions);
+        this.bg = new PBackground(this.bgOptions);
 
         this.initializeParticles(this.bg);
 
@@ -53,11 +55,20 @@ export default class extends Controller {
 
     updateBackground(event)
     {
+        //console.log(event.currentTarget.value)
+        this.bg.speedModValue = parseFloat(event.currentTarget.value);
+
+        console.log('parseFloat(event.currentTarget.value)',parseFloat(event.currentTarget.value));
+        console.log('this.bg.speedModValue',this.bg.speedModValue);
+
         this.initializeParticles(this.bg);
     }
 
     initializeParticles(bg) {
 
+        //bg.speedModValue = this.speedModValue;
+        console.log('initializeParticles bg.speedModValue',bg.speedModValue);
+        //console.log('this.speedModValue',this.speedModValue);
         /** initialize particles */
         bg.init();
     }
--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -18,7 +18,6 @@ export default class extends Controller {
     }
 
     clickOutside(event) {
-       // event.preventDefault()
         this.resultTarget.innerHTML = '';
     }
 
