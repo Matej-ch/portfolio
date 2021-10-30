13e65e07c83310392b62847cf819aada570469ec
Matej <matejchalachan@gmail.com>
2021-10-30 21:12:16 +0200

    close on escape

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -2,12 +2,38 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
 
-    static targets = ['modal','backdrop'];
+    static targets = ['modal','backdrop','close'];
+
+    connect()
+    {
+        window.addEventListener('keyup', this.hideOnKeyUp)
+    }
+
+    disconnect () {
+        window.removeEventListener('keyup', this.hideOnKeyUp)
+    }
+
+    hideOnKeyUp = (event) => {
+        if(event.code === 'Escape') {
+            this.modalTarget.classList.add("hidden");
+            this.backdropTarget.classList.add("hidden");
+            this.modalTarget.classList.remove("flex");
+            this.backdropTarget.classList.remove("flex");
+        }
+    }
 
     openModal(event) {
+        this.toggleModal();
+    }
+
+    closeModal(event) {
+        this.toggleModal();
+    }
+
+    toggleModal() {
         this.modalTarget.classList.toggle("hidden");
         this.backdropTarget.classList.toggle("hidden");
         this.modalTarget.classList.toggle("flex");
         this.backdropTarget.classList.toggle("flex");
-    }
+    };
 }
