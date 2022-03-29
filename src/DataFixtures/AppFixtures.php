cfa160d122e4ffbf36970febf7c6c8a8578ccb55
Matej <matejchalachan@gmail.com>
2022-03-29 21:23:47 +0200

    added admin factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,29 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Admin;
+use App\Factory\AdminFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture implements FixtureGroupInterface
 {
-    private $encoderFactory;
-
-    public function __construct(PasswordHasherFactoryInterface $encoderFactory)
-    {
-        $this->encoderFactory = $encoderFactory;
-    }
-
     public function load(ObjectManager $manager)
     {
-        $admin = new Admin();
-        $admin->setRoles(['ROLE_ADMIN']);
-        $admin->setUsername('admin');
-        $admin->setEmail('admin@example.com');
-        $admin->setPassword($this->encoderFactory->getPasswordHasher(Admin::class)->hash('admin12345'));
-        $manager->persist($admin);
+        AdminFactory::createOne(['email' => 'admin@example.com', 'username' => 'admin', 'roles' => ['ROLE_ADMIN']]);
 
         $manager->flush();
     }
