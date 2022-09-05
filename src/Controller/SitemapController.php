cdaa257d9be008162756324705f152063f8eeab5
Matej <matejchalachan@gmail.com>
2022-09-05 14:24:20 +0200

    added sitemap html

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -11,21 +11,19 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SitemapController extends AbstractController
 {
-    #[Route('/sitemap/sitemap.xml', name: 'sitemap', defaults: ['_format' => 'xml'])]
+    #[Route('/sitemap/sitemap', name: 'sitemap')]
     public function showAction(Request $request, ProjectRepository $repository, Packages $assetPackage): Response
     {
         $urls = [];
         $hostname = $request->getSchemeAndHttpHost();
 
-        // add static urls
-        $urls[] = ['loc' => $this->generateUrl('app_homepage')];
-        $urls[] = ['loc' => $this->generateUrl('app_about')];
+        $urls[] = ['loc' => $this->generateUrl('app_homepage'), 'label' => 'Projects homepage'];
+        $urls[] = ['loc' => $this->generateUrl('app_about'), 'label' => 'About me'];
 
-
-        // add dynamic urls, like blog posts from your DB
         foreach ($repository->findAll() as $project) {
             $urls[] = [
                 'loc' => $this->generateUrl('project_show', ['slug' => $project->getSlug()]),
+                'label' => $project->getName(),
                 'image' => [
                     'loc' => $assetPackage->getUrl("uploads/projects/" . $project->getBgImg()),
                     'title' => $project->getBgImg()
@@ -33,15 +31,28 @@ class SitemapController extends AbstractController
             ];
         }
 
-        // return response in XML format
-        $response = new Response(
-            $this->renderView('sitemap/sitemap.html.twig', [
-                'urls' => $urls,
-                'hostname' => $hostname]),
-            200
-        );
-        $response->headers->set('Content-Type', 'text/xml');
 
+        $type = $request->get('type');
+        if (empty($type)) {
+            $type = 'xml';
+        }
+
+        $response = new Response();
+        $response->setStatusCode(200);
+
+        if ($type === 'xml') {
+            $response->setContent($this->renderView('sitemap/xml.html.twig', [
+                'urls' => $urls,
+                'hostname' => $hostname]));
+
+            $response->headers->set('Content-Type', 'text/xml');
+
+        } else {
+            $response->setContent($this->renderView('sitemap/text.html.twig', [
+                'urls' => $urls,
+                'hostname' => $hostname]));
+        }
+        
         return $response;
     }
 }
