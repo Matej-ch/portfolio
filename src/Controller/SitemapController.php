26159228af7b446806992edf0f3b6e263b6cd72c
Matej <matejchalachan@gmail.com>
2022-09-26 00:29:31 +0200

    updated sitemap

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectCollectionRepository;
 use App\Repository\ProjectRepository;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,9 +13,9 @@ use Symfony\Component\HttpFoundation\Response;
 class SitemapController extends AbstractController
 {
     #[Route('/sitemap/sitemap.xml', name: 'app_sitemap_xml', defaults: ['xml'])]
-    public function showAsXml(Request $request, ProjectRepository $repository, Packages $assetPackage): Response
+    public function showAsXml(Request $request, ProjectRepository $repository, ProjectCollectionRepository $projectCollectionRepository, Packages $assetPackage): Response
     {
-        $urls = $this->parseUrls($repository, $assetPackage);
+        $urls = $this->parseUrls($repository, $projectCollectionRepository, $assetPackage);
 
         $response = new Response(
             $this->renderView('sitemap/xml.html.twig', [
@@ -28,9 +29,9 @@ class SitemapController extends AbstractController
     }
 
     #[Route('/sitemap', name: 'app_sitemap_html', defaults: ['html'])]
-    public function showAsHtml(Request $request, ProjectRepository $repository, Packages $assetPackage): Response
+    public function showAsHtml(Request $request, ProjectRepository $repository, ProjectCollectionRepository $projectCollectionRepository, Packages $assetPackage): Response
     {
-        $urls = $this->parseUrls($repository, $assetPackage);
+        $urls = $this->parseUrls($repository, $projectCollectionRepository, $assetPackage);
 
         return new Response(
             $this->renderView('sitemap/text.html.twig', [
@@ -42,14 +43,16 @@ class SitemapController extends AbstractController
 
     /**
      * @param ProjectRepository $repository
+     * @param ProjectCollectionRepository $projectCollectionRepository
      * @param Packages $assetPackage
      * @return array
      */
-    private function parseUrls(ProjectRepository $repository, Packages $assetPackage): array
+    private function parseUrls(ProjectRepository $repository, ProjectCollectionRepository $projectCollectionRepository, Packages $assetPackage): array
     {
         $urls = [];
 
         $urls[] = ['loc' => $this->generateUrl('app_homepage'), 'label' => 'Projects homepage'];
+        $urls[] = ['loc' => $this->generateUrl('app_collections'), 'label' => 'Projects collections'];
         $urls[] = ['loc' => $this->generateUrl('app_about'), 'label' => 'About me'];
 
         foreach ($repository->findAll() as $project) {
@@ -60,6 +63,14 @@ class SitemapController extends AbstractController
                     'loc' => $assetPackage->getUrl("uploads/projects/" . $project->getBgImg()),
                     'title' => $project->getBgImg()
                 ],
+            ];
+        }
+
+        foreach ($projectCollectionRepository->findAll() as $collection) {
+            $urls[] = [
+                'loc' => $this->generateUrl('app_collection_show', ['slug' => $collection->getSlug()]),
+                'label' => $collection->getName(),
+                'image' => [],
             ];
         }
 
