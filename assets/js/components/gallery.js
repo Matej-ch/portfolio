000bd09d06bb2c801ce0b4c8301ec45c1b643e02
Matej <matejchalachan@gmail.com>
2022-10-22 19:31:03 +0200

    added captions and titles in gallery

--- assets/js/components/gallery.js
+++ assets/js/components/gallery.js
@@ -11,9 +11,38 @@ window.onload = function () {
             secondaryZoomLevel: 1,
             maxZoomLevel: 1,
             bgOpacity: 0.2,
-            padding: {top: 20, bottom: 40, left: 100, right: 100},
+            padding: {top: 20, bottom: 40, left: 50, right: 50},
             pswpModule: () => import('photoswipe')
         });
+
+        lightbox.on('uiRegister', function () {
+            lightbox.pswp.ui.registerElement({
+                name: 'custom-caption',
+                order: 9,
+                isButton: false,
+                appendTo: 'root',
+                html: 'Caption text',
+                onInit: (el, pswp) => {
+                    lightbox.pswp.on('change', () => {
+                        const currSlideElement = lightbox.pswp.currSlide.data.element;
+                        let captionHTML = '';
+                        if (currSlideElement) {
+                            const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+                            if (hiddenCaption) {
+                                // get caption from element with class hidden-caption-content
+                                captionHTML = hiddenCaption.innerHTML;
+                            } else {
+                                // get caption from alt attribute
+                                captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+                            }
+                        }
+                        el.innerHTML = captionHTML || '';
+                    });
+                }
+            });
+        });
+
+
         lightbox.init();
     }
 }
